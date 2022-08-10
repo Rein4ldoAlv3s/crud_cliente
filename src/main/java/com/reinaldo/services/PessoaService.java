c516c24b383b277a43c60e7fe2e7936216f4811e
Reinaldo <naldos15@hotmail.com>
2022-08-09 23:59:28 -0300

    path update

--- src/main/java/com/reinaldo/services/PessoaService.java
+++ src/main/java/com/reinaldo/services/PessoaService.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import com.reinaldo.domain.Pessoa;
@@ -23,36 +22,47 @@ public class PessoaService {
 
 	public Pessoa findByID(Integer id) {
 		Optional<Pessoa> obj = repository.findById(id);
-		return obj.orElseThrow(() -> new NullPointerException
-				("Objeto não encontrado! ID: " + id + " Objeto: " + Pessoa.class.getName()));
+		return obj.orElseThrow(() -> new NullPointerException(
+				"Objeto não encontrado! ID: " + id + " Objeto: " + Pessoa.class.getName()));
 	}
 
 	public List<Pessoa> findAll() {
 		return repository.findAll();
 	}
 
-	
 	public PessoaDTO save(Pessoa p) {
-	 	Pessoa objCPF = findByCPF(p.getCpf());
-		
-	 	if(objCPF == null) {
-	 		Pessoa obj = repository.save(p);
-	 		PessoaDTO objDTO = new PessoaDTO(obj);
-	 		return objDTO;
-	 	}
-	 	
-	 	throw new DataIntegrityException("Informe um CPF diferente!");
-		
+		Pessoa objCPF = findByCPF(p.getCpf());
+
+		if (objCPF == null) {
+			Pessoa obj = repository.save(p);
+			PessoaDTO objDTO = new PessoaDTO(obj);
+			return objDTO;
+		}
+
+		throw new DataIntegrityException("Informe um CPF diferente!");
+
 	}
-	
+
+	public Pessoa update(Integer id, @Valid Pessoa pessoaObj) {
+		Pessoa oldObj = findByID(id);
+		
+		if(findByCPF(pessoaObj.getCpf()) != null && findByCPF(pessoaObj.getCpf()).getId() != id) {
+			throw new DataIntegrityException("CPF já cadastrado na base de dados");
+		}
+		
+		oldObj.setCpf(pessoaObj.getCpf());
+		oldObj.setNome(pessoaObj.getNome());
+		oldObj.setTelefone(pessoaObj.getTelefone());
+		return repository.save(oldObj);
+	}
+
 	public Pessoa findByCPF(String cpf) {
 		Pessoa obj = repository.findByCPF(cpf);
-		
-		if(obj != null) {
+
+		if (obj != null) {
 			return obj;
 		}
 		return null;
 	}
-	
-	
+
 }
